e3a77471241e2b75d59da57cda5aab4ff6800fff
Trisha Lim <limtrishajean@gmail.com>
2021-09-25 13:09:28 +0700

    handle change of form input

--- src/FormInput.js
+++ src/FormInput.js
@@ -1,12 +1,22 @@
 import './FormInput.css';
+import { useState } from 'react';
 
 function FormInput(props) {
   const { label } = props;
 
+  const [value, setValue] = useState('');
+  const onChange = (event) => {
+    setValue(event.target.value);
+  };
+
   return (
     <div className="FormInput">
       <label>{label}</label>
-      <input type="text"></input>
+      <input
+        type="text" 
+        value={value}
+        onChange={onChange}
+      />
     </div>
   )
 }
